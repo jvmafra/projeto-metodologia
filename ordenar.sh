df3d0061ebc6e798c990f5fc77d0c515b8f5e2bf
gileadekelvin <gileadekelvin@gmail.com>
2017-08-07 20:58:47 -0300

    Ajustando script

--- ordenar.sh
+++ ordenar.sh
@@ -1,11 +1,10 @@
-cd src
-javac Main.java
+javac src/*.java
 
 if [[ -z "$2" && -z "$3" ]]; then
   echo Assuming input.txt and output.txt
-  java Main $1 ../input.txt ../output.txt
+  java -cp ./src Main $1 input.txt output.txt
 elif [ -z "$3" ]; then
   echo Missing argument. Try like: $ ./ordenar.sh merge input.txt output.txt
 else
-  java Main $1 ../$2 ../$3
+  java -cp ./src Main $1 $2 $3
 fi
